0e09ee26809b95fa80b76b9e0ed91f559df3a215
Laura Atencio <lauraatenciob@gmail.com>
2024-01-23 17:19:44 -0500

    style completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='app'>
+      <div id='calculator'>
+        <div id='screen'>
+          <p id='formulaScreen'>0+0</p>
+          <p id='display'>0</p>
+        </div>
+        <div id='buttons'>
+          <button id='clear'><i class="fa-solid fa-a"></i><i class="fa-solid fa-c"></i></button>
+          <button className="operator" id='divide'><i class="fa-solid fa-divide"></i></button>
+          <button className="operator" id='multiply'><i class="fa-solid fa-x"></i></button>
+          <button className="number" id='seven'>7</button>
+          <button className="number" id='eight'>8</button>
+          <button className="number" id='nine'>9</button>
+          <button className="operator" id='subtract'><i class="fa-solid fa-minus"></i></button>
+          <button className="number" id='four'>4</button>
+          <button className="number" id='five'>5</button>
+          <button className="number" id='six'>6</button>
+          <button className="operator" id='add'><i class="fa-solid fa-plus"></i></button>
+          <button className="number" id='one'>1</button>
+          <button className="number" id='two'>2</button>
+          <button className="number" id='three'>3</button>
+          <button className="number" id='zero'>0</button>
+          <button id='decimal'>.</button>
+          <button id='equals'><i class="fa-solid fa-equals"></i></button>
+        </div>
+      </div>
     </div>
   );
 }
